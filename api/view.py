d1a21a6cb77d8b6b269edd559623f8c87c98387e
RaghadAbdulhadi <raghadabdulhadi1999@gmail.com>
2022-04-11 22:42:49 +0300

    coding

--- api/view.py
+++ api/view.py
@@ -1,12 +1,15 @@
 import logging
-def main(request):
-    request_json = request.get_json()
-    headers = {
-        'Access-Control-Allow-Origin': '*'
-    }
-    if request.args and 'message' in request.args:
-        logging.info(request.args.get('message'))
-        logging.info('value is found in requests.args')
-        return (request.args.get('message'), 200, headers)
-    else:
-        return ('Hello World!', 200, headers)
+from http.server import BaseHTTPRequestHandler
+from urllib import parse
+class handler(BaseHTTPRequestHandler):
+    def main(self):
+        request_json = self.get_json()
+        headers = {
+            'Access-Control-Allow-Origin': '*'
+        }
+        if self.args and 'message' in self.args:
+            logging.info(self.args.get('message'))
+            logging.info('value is found in selfs.args')
+            return (self.args.get('message'), 200, headers)
+        else:
+            return ('Hello World!', 200, headers)
