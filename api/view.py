59a06487723dbe93e5041925dd85c67fc243297e
RaghadAbdulhadi <raghadabdulhadi1999@gmail.com>
2022-04-11 22:39:56 +0300

    coding

--- api/view.py
+++ api/view.py
@@ -1,33 +1,12 @@
-from http.server import BaseHTTPRequestHandler
-import webbrowser
-
-class handler(BaseHTTPRequestHandler):
-
-  def do_GET(self):
-
-        # to open/create a new html file in the write mode
-    f = open('view.html', 'w')
-    
-    # the html code which will go in the file GFG.html
-    html_template = """<html>
-    <head>
-    <title>Title</title>
-    </head>
-    <body>
-    <h2>Welcome To GFG</h2>
-    
-    <p>Default code has been loaded into the Editor.</p>
-    
-    </body>
-    </html>
-    """
-    
-    # writing the code into the file
-    f.write(html_template)
-    
-    # close the file
-    f.close()
-    self.send_response(200)
-    self.send_header('Content-type', 'text/html')
-    webbrowser.open('view.html') 
-    return
+import logging
+def main(request):
+    request_json = request.get_json()
+    headers = {
+        'Access-Control-Allow-Origin': '*'
+    }
+    if request.args and 'message' in request.args:
+        logging.info(request.args.get('message'))
+        logging.info('value is found in requests.args')
+        return (request.args.get('message'), 200, headers)
+    else:
+        return ('Hello World!', 200, headers)
