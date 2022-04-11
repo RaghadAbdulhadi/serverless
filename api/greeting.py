590c6ff37b1aa6cd0d8af7e6a97213f3978c4d3c
RaghadAbdulhadi <raghadabdulhadi1999@gmail.com>
2022-04-11 16:03:57 +0300

    greeting

--- api/greeting.py
+++ api/greeting.py
@@ -1,9 +1,22 @@
 from http.server import BaseHTTPRequestHandler
+from urllib import parse
 
 class handler(BaseHTTPRequestHandler):
 
   def do_GET(self):
     #  request is successful and you will get your response
+    s = self.path
+    url_components = parse.urlsplit(s)
+    query_string_list = parse.parse_qsl(url_components.query)
+    dic = dict(query_string_list)
+    name = dic.get('name')
+
+    if name:
+        message = f"Hello {name}"
+    else:
+        message = f"Hello No body"
+
+    message += f"\n Hello to our website"
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
