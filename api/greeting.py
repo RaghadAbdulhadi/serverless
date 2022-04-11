afcab8c8364fc5c8fd89a50a2de286caa5307a04
RaghadAbdulhadi <raghadabdulhadi1999@gmail.com>
2022-04-11 15:54:48 +0300

    greeting

--- api/greeting.py
+++ api/greeting.py
@@ -3,9 +3,10 @@ from http.server import BaseHTTPRequestHandler
 class handler(BaseHTTPRequestHandler):
 
   def do_GET(self):
+    #  request is successful and you will get your response
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
-    message = "Welcome to our website"
+    message = "Hello Amman"
     self.wfile.write(message.encode())
     return
