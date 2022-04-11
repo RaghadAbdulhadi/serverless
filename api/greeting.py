621f57b40ff7084adba492c251a7552e5b472e13
RaghadAbdulhadi <raghadabdulhadi1999@gmail.com>
2022-04-11 16:06:18 +0300

    greeting

--- api/greeting.py
+++ api/greeting.py
@@ -20,6 +20,5 @@ class handler(BaseHTTPRequestHandler):
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
-    message = "Hello Amman"
     self.wfile.write(message.encode())
     return
